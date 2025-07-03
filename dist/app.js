49ab1b697f19ea54c00ca3e53ba9eedac74c3e9c
unknown <abdoutonzar@gmail.com>
2025-07-03 15:57:38 +0100

    Ajout du dossier dist compilé

---
+++ dist/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("./connexion");
+const auth_route_1 = __importDefault(require("./routes/auth.route"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use('/api', auth_route_1.default);
+const port = process.env.PORT || 5001; // utilise celui de Railway en priorité
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
